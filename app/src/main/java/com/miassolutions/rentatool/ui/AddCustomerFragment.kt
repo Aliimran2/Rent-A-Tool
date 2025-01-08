6877c1f79579a06a82f6ca9a1888d6ee557568ff
MIAS <muhammadimran.math@gmail.com>
2025-01-08 23:13:00 +0500

    fragment classes added and nav graph setup

---
+++ app/src/main/java/com/miassolutions/rentatool/ui/AddCustomerFragment.kt
@@ -0,0 +1,7 @@
+package com.miassolutions.rentatool.ui
+
+import androidx.fragment.app.Fragment
+import com.miassolutions.rentatool.R
+
+class AddCustomerFragment : Fragment(R.layout.fragment_add_customer) {
+}
