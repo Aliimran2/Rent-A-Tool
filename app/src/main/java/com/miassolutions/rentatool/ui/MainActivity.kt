29d34b1b377e1cd05f0354a2f295675735d0e921
MIAS <muhammadimran.math@gmail.com>
2025-01-09 00:02:10 +0500

    main activity all setup

--- app/src/main/java/com/miassolutions/rentatool/ui/MainActivity.kt
+++ app/src/main/java/com/miassolutions/rentatool/ui/MainActivity.kt
@@ -1,7 +1,15 @@
 package com.miassolutions.rentatool.ui
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.miassolutions.rentatool.R
 import com.miassolutions.rentatool.databinding.ActivityMainBinding
 
 
@@ -11,9 +19,44 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.toolbar)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.stockFragment,
+                R.id.customersListFragment,
+                R.id.addCustomerFragment,
+                R.id.addToolFragment
+            ), binding.drawerLayout
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+        binding.navView.setupWithNavController(navController)
+
+val aboutApp = binding.navView.menu.findItem(R.id.aboutApp)
+        aboutApp.setOnMenuItemClickListener {
+            Toast.makeText(this@MainActivity, "MIAS Solutions", Toast.LENGTH_SHORT).show()
+            binding.drawerLayout.closeDrawers()
+            true
+        }
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
