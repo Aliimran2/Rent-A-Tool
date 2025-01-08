afbb9bcb748ac05ba08d4124e93bd3042436d31d
MIAS <muhammadimran.math@gmail.com>
2025-01-08 17:14:53 +0500

    theming and work on StockFragment layout

---
+++ app/src/main/java/com/miassolutions/rentatool/ui/StockFragment.kt
@@ -0,0 +1,8 @@
+package com.miassolutions.rentatool.ui
+
+import androidx.fragment.app.Fragment
+import com.miassolutions.rentatool.R
+
+class StockFragment: Fragment(R.layout.fragment_stock) {
+
+}
