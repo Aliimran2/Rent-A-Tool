95e33efdb35c91c56686a4608272ecd480a8724f
MIAS <muhammadimran.math@gmail.com>
2025-01-09 00:02:10 +0500

    main activity all setup

--- app/src/main/java/com/miassolutions/rentatool/ui/StockFragment.kt
+++ app/src/main/java/com/miassolutions/rentatool/ui/StockFragment.kt
@@ -1,8 +1,40 @@
 package com.miassolutions.rentatool.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
 import com.miassolutions.rentatool.R
+import com.miassolutions.rentatool.databinding.FragmentStockBinding
 
 class StockFragment: Fragment(R.layout.fragment_stock) {
+
+    private var _binding : FragmentStockBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentStockBinding.bind(view)
+
+        val options = NavOptions.Builder()
+            .setEnterAnim(R.anim.slide_in_right)   // Enter animation
+            .setExitAnim(R.anim.slide_out_left)    // Exit animation
+            .setPopEnterAnim(R.anim.slide_in_left) // Pop Enter animation
+            .setPopExitAnim(R.anim.slide_out_right) // Pop Exit animation
+            .build()
+
+        binding.rentButton.setOnClickListener {
+            findNavController().navigate(R.id.rentToolFragment, null, options)
+        }
+
+
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
 }
