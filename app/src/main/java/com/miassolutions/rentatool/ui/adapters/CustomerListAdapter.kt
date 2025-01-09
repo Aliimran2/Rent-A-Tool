711b0ad6e1efa29e8cb335d08149e1f5fb99a1f4
MIAS <muhammadimran.math@gmail.com>
2025-01-09 22:17:52 +0500

    CustomerListAdapter defined

--- app/src/main/java/com/miassolutions/rentatool/ui/adapters/CustomerListAdapter.kt
+++ app/src/main/java/com/miassolutions/rentatool/ui/adapters/CustomerListAdapter.kt
@@ -1,22 +1,44 @@
 package com.miassolutions.rentatool.ui.adapters
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.miassolutions.rentatool.data.model.Customer
+import com.miassolutions.rentatool.databinding.ItemCustomerBinding
 import com.miassolutions.rentatool.databinding.ItemDropDownCustomerBinding
 
 class CustomerListAdapter(
-    val itemClickListener : (Customer) -> Unit
-) : ListAdapter<Customer, CustomerListAdapter.CustomerVH>(CustomerDiffUtil()){
+    val itemClickListener: (Customer) -> Unit
+) : ListAdapter<Customer, CustomerListAdapter.CustomerVH>(CustomerDiffUtil()) {
 
 
-    inner class CustomerVH(val binding : ItemDropDownCustomerBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class CustomerVH(private val binding: ItemCustomerBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(customer: Customer){
+        fun bind(customer: Customer) {
             binding.apply {
+                tvCustomerName.text = customer.name
+                tvConstructionPlace.text = "Mock City"
+                tvCustomerPhone.text = customer.contact
+                ivPhone.setOnClickListener {
+                    Toast.makeText(root.context, "Calling function handle later", Toast.LENGTH_SHORT).show()
+                }
+                ivCustomer.setOnClickListener {
+                    itemClickListener(customer)
+                }
 
             }
         }
 
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomerVH {
+        val inflater = LayoutInflater.from(parent.context)
+        val mBinding = ItemCustomerBinding.inflate(inflater, parent, false)
+        return CustomerVH(mBinding)
+    }
+
+    override fun onBindViewHolder(holder: CustomerVH, position: Int) = holder.bind(getItem(position))
 }
