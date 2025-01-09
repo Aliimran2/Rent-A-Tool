d9d1785dcab170762493fb31d81056fc8ee39c43
MIAS <muhammadimran.math@gmail.com>
2025-01-09 21:37:21 +0500

    rectify ids and some changes in ui

--- app/src/main/java/com/miassolutions/rentatool/ui/fragments/UpdateDeleteCustomerFragment.kt
+++ app/src/main/java/com/miassolutions/rentatool/ui/fragments/UpdateDeleteCustomerFragment.kt
@@ -3,5 +3,5 @@ package com.miassolutions.rentatool.ui.fragments
 import androidx.fragment.app.Fragment
 import com.miassolutions.rentatool.R
 
-class UpdateDeleteCustomerFragment : Fragment(R.layout.fragment_update_delete_customer) {
+class UpdateDeleteCustomerFragment : Fragment(R.layout.fragment_update_customer) {
 }
