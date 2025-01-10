8b02338f8c2d0234eae85b179184316195ca59db
MIAS <muhammadimran.math@gmail.com>
2025-01-10 20:10:41 +0500

    basic fragment setup

--- app/src/main/java/com/miassolutions/rentatool/ui/fragments/AddCustomerFragment.kt
+++ app/src/main/java/com/miassolutions/rentatool/ui/fragments/AddCustomerFragment.kt
@@ -1,7 +1,29 @@
 package com.miassolutions.rentatool.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.miassolutions.rentatool.R
+import com.miassolutions.rentatool.databinding.FragmentAddCustomerBinding
+import com.miassolutions.rentatool.ui.viewmodels.RentalViewModel
 
 class AddCustomerFragment : Fragment(R.layout.fragment_add_customer) {
+
+    private var _binding : FragmentAddCustomerBinding? = null
+    private val binding get() = _binding!!
+
+    private val rentalViewModel : RentalViewModel by activityViewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentAddCustomerBinding.bind(view)
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
