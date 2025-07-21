584b4d7a54d9f6f944ef225e5648b2d4395f3362
Ali Imran <muhammadimran.math@gmail.com>
2025-07-21 11:08:07 +0500

    Upgrade AGP to 8.11.1 and various libraries versions and sdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.miassolutions.rentatool"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.miassolutions.rentatool"
-        minSdk = 24
-        targetSdk = 35
+        minSdk = 26
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -56,41 +56,37 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
     // Navigation Component
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.5") // Latest stable version as of December 11, 2024 :contentReference[oaicite:1]{index=1}
-    implementation("androidx.navigation:navigation-ui-ktx:2.8.5") // Latest stable version as of December 11, 2024 :contentReference[oaicite:2]{index=2}
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.9.2")
 
     // Lifecycle
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7") // Latest stable version as of December 11, 2024 :contentReference[oaicite:3]{index=3}
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7") // Latest stable version as of December 11, 2024 :contentReference[oaicite:4]{index=4}
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.2")
 
     // Room Database
-    implementation("androidx.room:room-runtime:2.6.1") // Assumed latest stable version
-    implementation("androidx.room:room-ktx:2.6.1") // Assumed latest stable version
-    ksp("androidx.room:room-compiler:2.6.1") // Assumed latest stable version
+    implementation("androidx.room:room-runtime:2.7.2")
+    implementation("androidx.room:room-ktx:2.7.2")
+    ksp("androidx.room:room-compiler:2.7.2")
 
-    // Networking
-    implementation("com.squareup.retrofit2:retrofit:2.11.0") // Assumed latest stable version
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0") // Assumed latest stable version
-    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0") // Assumed latest stable version
+
 
     // Image Loading
     implementation("com.github.bumptech.glide:glide:4.16.0") // Assumed latest stable version
     ksp("com.github.bumptech.glide:compiler:4.16.0")
 
     // WorkManager
-    implementation("androidx.work:work-runtime-ktx:2.10.0") // Assumed latest stable version
+    implementation("androidx.work:work-runtime-ktx:2.10.2") // Assumed latest stable version
 
     // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3") // Assumed latest stable version
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2") // Assumed latest stable version
 
 
     //hilt
-    implementation("com.google.dagger:hilt-android:2.51.1")
-    kapt("com.google.dagger:hilt-android-compiler:2.51.1")
+    implementation("com.google.dagger:hilt-android:2.57")
+    kapt("com.google.dagger:hilt-android-compiler:2.57")
 
     implementation ("de.hdodenhof:circleimageview:3.1.0") // CircleImageView library
 
-    implementation ("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.16.0")
 
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,10 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     //ksp plugin in project level
-    id("com.google.devtools.ksp") version "2.1.0-1.0.29"
+    id("com.google.devtools.ksp") version "2.2.0-2.0.2"
 
     //hilt
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    id("com.google.dagger.hilt.android") version "2.57" apply false
 }
 
 buildscript {
