7a0e2ba6eb8863a74c2171ab5959842c018d84c4
Ali Imran <muhammadimran.math@gmail.com>
2025-07-24 13:07:40 +0500

    database reset from the settingsFragment

--- app/src/main/java/com/miassolutions/rentatool/ui/fragments/drawerfragments/SettingsFragment.kt
+++ app/src/main/java/com/miassolutions/rentatool/ui/fragments/drawerfragments/SettingsFragment.kt
@@ -1,7 +1,46 @@
 package com.miassolutions.rentatool.ui.fragments.drawerfragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.miassolutions.rentatool.R
+import com.miassolutions.rentatool.databinding.FragmentSettingsBinding
+import com.miassolutions.rentatool.ui.viewmodel.MainViewModel
+import com.miassolutions.rentatool.utils.extenstions.showConfirmDialog
+import com.miassolutions.rentatool.utils.extenstions.showToast
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
+
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel by viewModels<MainViewModel>()
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentSettingsBinding.bind(view)
+
+        binding.resetBtn.setOnClickListener {
+            showConfirmDialog(
+                title = "Confirmation",
+                message = "Are you sure to reset database",
+                positiveText = "Yes, sure",
+                onConfirm = {
+                    viewModel.resetAllDatabase()
+                    showToast("Database reset")
+                }
+            )
+
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
